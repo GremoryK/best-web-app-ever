96ffcef7f11e163b17c3d09ac4536d50c6003579
GremoryK <ruslankim7287@gmail.com>
2023-05-06 00:45:13 +0300

    PM commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import MyHeader from "./components/UI/Header/MyHeader";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MyHeader />
+      </header>
+    </div>
+  );
+}
+
+export default App;

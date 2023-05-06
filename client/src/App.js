63ef997dd65f40b250a493edba91e93771b57747
GremoryK <ruslankim7287@gmail.com>
2023-05-06 12:57:09 +0300

    PM commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import MyHeader from "./components/UI/Header/MyHeader";
+import MyCarousel from "./components/UI/Carousel/MyCarousel";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <MyHeader />
+          <MyCarousel />
       </header>
     </div>
   );
--- client/src/components/UI/Header/MyHeader.jsx
+++ client/src/components/UI/Header/MyHeader.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import {Container, Grid, Typography} from "@mui/material";
+import {Grid, Typography} from "@mui/material";
 import MyBreadcrumbs from "../Breadcrumbs/MyBreadcrumbs";
 import classes from '../../../assets/MyLogo.module.css'
+import {Container} from "react-bootstrap";
 
 const MyHeader = () => {
     return (
         <header style={{backgroundColor: "#232536", padding: "20px 0px"}}>
             <Container >
                 <Grid container spacing={2} alignItems="center">
-                    <Grid item xs={3}> <Typography className={classes.MyLogo}> IT STEP </Typography> </Grid>
+                    <Grid item xs={1}></Grid>
+                    <Grid item xs={2}> <Typography className={classes.MyLogo}> IT STEP </Typography> </Grid>
                     <Grid item xs={6}></Grid>
                     <Grid item xs={3}><MyBreadcrumbs /></Grid>
                 </Grid>

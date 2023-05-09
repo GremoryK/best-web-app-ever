236129aecca86b4ae1f63ba48d2f7e5b2638e508
Robez228 <robez16@mail.ru>
2023-05-09 23:43:50 +0300

    Edit app.js

--- server/app.js
+++ server/app.js
@@ -1,11 +1,22 @@
+require('dotenv').config()
+
 const express = require('express')
+const sequelize = require('db')
+
+const PORT = process.env.PORT || 5000
 const app = express()
-const PORT = 5000
 
-app.get('/', (req, res) => {
-    res.send('Hello Mom!')
-})
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`)
-})
+        app.listen(PORT, () => {
+            console.log(`Server listening on port ${PORT}`)
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()

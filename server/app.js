1c7ecf84a65d81847a563ebad780cda448d217b3
GremoryK <ruslankim7287@gmail.com>
2023-05-10 00:41:44 +0300

    PM Commit

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const sequelize = require('db')
-
+const models = require('/src/models/models')
 const PORT = process.env.PORT || 5000
 const app = express()
 

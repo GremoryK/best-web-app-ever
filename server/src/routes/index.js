6b339805c8ca741bc78a95613f42d25335ece5b7
Robez228 <robez16@mail.ru>
2023-05-13 23:39:07 +0300

    Edit main router

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,9 +4,8 @@ const userRouter = require('./userRouter')
 const articleRouter = require('./articleRouter')
 const homeRouter = require('./homeRouter')
 
+router.use('/home', homeRouter)
 router.use('/user', userRouter)
 router.use('/article', articleRouter)
-router.use('/home', homeRouter)
 
 module.exports = router
-

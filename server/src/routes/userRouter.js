5fa91f2668d640f8a7b1161e92ab3dc5e1c15b83
Robez228 <robez16@mail.ru>
2023-05-13 23:38:41 +0300

    Edit userRouter.js

--- server/src/routes/userRouter.js
+++ server/src/routes/userRouter.js
@@ -1,11 +1,15 @@
 const Router = require('express')
 const router = new Router()
 
-router.post('/registration', )
-router.post('/login', )
+router.post('/registration', (req, res) => {
+    res.json({message: 'User: Registration'})
+})
+router.post('/login', (req, res) => {
+    res.json({message: 'User: Login'})
+})
+
 router.get('/auth', (req, res) => {
-    res.json({message: 'All working'})
+    res.json({message: 'User: Auf'})
 })
 
 module.exports = router
-

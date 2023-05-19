530af032ef8d1cbafb27492e355f1c05e44c580c
Robez228 <robez16@mail.ru>
2023-05-20 00:00:15 +0300

    Edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-import MyHeader from "./components/UI/Header/MyHeader";
-import MyCarousel from "./components/UI/Carousel/MyCarousel";
-import MyCard1 from "./components/UI/Card/MyCard1";
+import MyHeader from "./components/UI/Header/MyHeader"
+import MyCarousel from "./components/UI/Carousel/MyCarousel"
+import MyCard1 from "./components/UI/Card/MyCard1"
+import Editor from "./components/UI/Editor/Editor";
 
 function App() {
   return (
@@ -8,8 +9,9 @@ function App() {
         <MyHeader />
         <MyCarousel />
         <MyCard1 />
+        <Editor />
     </div>
-  );
+  )
 }
 
 export default App;

7651af53c784d40a151dbcabceb59c4a6ccb3759
Robez228 <robez16@mail.ru>
2023-05-19 22:24:04 +0300

    Edit app

--- server/app.js
+++ server/app.js
@@ -1,15 +1,18 @@
 require('dotenv').config()
 
 const express = require('express')
-const sequelize = require('./db')
-const models = require('./src/models/models')
-const cors = require('cors')
 const router = require('./src/routes/index')
+
+const sequelize = require('./db')
+require('./src/models/models')
+
+const cors = require('cors')
 const errorHandler = require('./src/middleware/ErrorHandlingMiddleware')
 const fileUpload = require('express-fileupload')
 
 const PORT = process.env.PORT || 5000
 const app = express()
+
 app.use(cors())
 app.use(express.json())
 app.use(fileUpload({}))

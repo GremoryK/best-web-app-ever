1d1a4da9c3efcb0b039920125ee1a71ca15d6e35
GremoryK <ruslankim7287@gmail.com>
2023-05-20 11:21:21 +0300

    PM Commit edit articleRouter.js edit userRouter.js add authMiddleware

--- server/src/routes/articleRouter.js
+++ server/src/routes/articleRouter.js
@@ -2,7 +2,7 @@ const Router = require('express')
 const router = new Router()
 const articleController = require('../controllers/articleController')
 
-router.post('/create', articleController.createOne)
+router.post('/create', articleController.create)
 router.get('/get', articleController.getAll)
 router.delete('/delete',articleController.delete)
 
--- server/src/routes/userRouter.js
+++ server/src/routes/userRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express')
 const router = new Router()
 const UserControllers = require('../controllers/userController')
+const authMiddleware = require('../middleware/authMiddleware')
 
 router.post('/registration', UserControllers.registration)
 router.post('/login', UserControllers.login)
-router.get('/auth', UserControllers.check)
+router.get('/auth', authMiddleware, UserControllers.check)
 
 module.exports = router

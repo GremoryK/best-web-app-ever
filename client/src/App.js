518f68043c670824a4e6fc0ada98c984ca5c2468
GremoryK <ruslankim7287@gmail.com>
2023-06-03 10:58:06 +0300

    edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
 import MyNavbar from "./components/MyNavbar";
-import Home from "./pages/Home";
 import MyFooter from "./components/MyFooter";
 import {Container} from "react-bootstrap";
 
